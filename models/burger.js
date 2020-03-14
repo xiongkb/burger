dccd9eaf4cdc66a7d488d70f04a1c6743997e4a6
xiongkb <mkan-bx@hotmail.com>
2020-03-13 21:25:32 -0700

    added code to server to run the app

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,16 @@
-const orm = require("../config/orm.js")
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: {
+        orm.all()
+    },
+    create: {
+        orm.create()
+    },
+    update: {
+        orm.update()
+    },
+    delete: {
+        
+    }
+};
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+// setting the variables for express and handlebars
 const express = require("express");
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.satic("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(xpress.json());
+
+const handlebar = require("express-handlebars");
+
+app.engine("handlebars", handlebar({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// importing the routes
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// adding a listen to we know it's there
+app.listen(PORT, function() {
+    console.log("App is listening on localhost:" + PORT);
+});
